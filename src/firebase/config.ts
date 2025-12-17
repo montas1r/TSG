e52b1a06a6f79dbaccfc9a6a3e01affd5711d8de
Nothing Here <nhere796@gmail.com>
2025-12-17 19:05:04 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2609478921-bca97",
+  "appId": "1:869279012352:web:7f5722291f6a53c7caaae5",
+  "apiKey": "AIzaSyD7lfBHDKuJAPRO3ev5QuuYtU_-YzY0_ZI",
+  "authDomain": "studio-2609478921-bca97.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "869279012352"
+};
